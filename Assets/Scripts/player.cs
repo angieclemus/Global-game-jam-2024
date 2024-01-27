9d5e8449e10ff51aaffd9aeebf6a9eee0a547a12
angieclemus <angielem11@gmail.com>
2024-01-26 20:04:10 -0500

    movimiento y activación de la caja

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //1. Declaración de variables
+    [Range(1, 10)] public float velocidad; //Velocidad del jugador
+    Rigidbody2D rb2d;
+    bool isPressed = true;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Start () {
+        //2. Capturo y asocio los componentes Rigidbody2D y Sprite Renderer del Jugador
+        rb2d = GetComponent<Rigidbody2D>();
     }
+   
+    void FixedUpdate () {
+
+    //3. Movimiento horizontal
+    float movimientoH = Input.GetAxisRaw("Horizontal");
+    rb2d.velocity = new Vector2(movimientoH * velocidad, rb2d.velocity.y);
+
+    //4. Movimiento vertical
+    float movimientoV = Input.GetAxisRaw("Vertical");
+    rb2d.velocity = new Vector2(rb2d.velocity.x, movimientoV * velocidad);
+
+    //5. Accionar caja (space)
+   if(Input.GetKeyUp(KeyCode.Space) ){
+    activarCaja();
+   }
+   
+   }
+
+   void activarCaja(){
+
+   }
 }
