d0b5226453e9ad5a3cdf3ff20bd8b0c8fe853301
angieclemus <angielem11@gmail.com>
2024-01-27 18:14:30 -0500

    dirección del personaje, cambios de diseño, barra, contador

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public player player;
+    public Slider barra;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,6 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+     barra.value = player.tiempoTranscurrido;   
     }
 }
